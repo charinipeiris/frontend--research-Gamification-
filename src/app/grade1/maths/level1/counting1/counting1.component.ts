bcc40d6cad82e172478455a15eb03a23ffa03ceb
charinipeiris <30485000+charinipeiris@users.noreply.github.com>
2020-08-03 01:50:06 +0530

    counting 1 introduction part 1
    counting 1 introduction part 1

--- src/app/grade1/maths/level1/counting1/counting1.component.ts
+++ src/app/grade1/maths/level1/counting1/counting1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+declare function test(): any;
+declare function start(): any;
 @Component({
   selector: 'app-counting1',
   templateUrl: './counting1.component.html',
@@ -10,6 +11,9 @@ export class Counting1Component implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    test();
+    
+  
+   
   }
-
 }
---
+++ src/assets/js/counting1.js
@@ -0,0 +1,86 @@
+
+    var a =0;
+    var m1 = 1000;
+    var m2 = 1000;
+    var m3 = 1000;
+    var m4 = 1000;
+    var m5 = 1000;
+    function test(){document.f1.txt1.style.visibility='hidden';
+    document.f1.txt3.style.visibility='hidden';
+    document.f3.clip1.style.visibility='hidden';
+    a++;
+    document.f1.txt1.value=a;
+    if(a==6){document.f1.txt1.value=""; a="0";
+    document.f.but1.disabled="disabled"; 
+    window.setTimeout("start();", 0);
+    document.f2.clip2.style.left=1000;
+    document.f2.clip3.style.left=1000; 
+    document.f2.clip4.style.left=1000;
+    document.f2.clip5.style.left=1000;
+    document.f2.clip6.style.left=1000;
+    document.f1.txt3.value=" "; document.f3.clip1.src="/assets/images/b.jpg"; }
+    
+        if(document.f1.txt1.value<=5 && document.f1.txt1.value>0){
+            document.f.but1.disabled="disabled";
+            start();
+            setTimeout("document.f.but1.disabled=false,document.f1.txt1.style.visibility='visible',document.f1.txt3.style.visibility='visible',document.f3.clip1.style.visibility='visible'",2500);
+            
+        }
+            
+    }
+    
+    
+    function start(){
+        if(document.f1.txt1.value==1){ 
+            document.f1.txt3.value='එක ';
+            document.f2.clip2.style.left=m1;
+            m1-=2;}
+        if(m1>320){
+            window.setTimeout("start();", 0);
+            document.f3.clip1.src="/assets/images/1.gif";
+}
+           
+                
+    
+        if(document.f1.txt1.value==2){ 
+    document.f1.txt3.value="දෙක ";
+    
+            document.f2.clip3.style.left=m2;
+               m2-=2;
+               if(m2>370)
+                     window.setTimeout("start();", 0);
+    document.f3.clip1.src="/assets/images/2.gif";
+        }
+    
+        if(document.f1.txt1.value==3){ 
+    document.f1.txt3.value="තුන ";
+    
+            document.f2.clip4.style.left=m3;
+               m3-=2;
+               if(m3>430)
+                     window.setTimeout("start();", 0);
+    document.f3.clip1.src="/assets/images/3.gif";
+        }
+    
+    if(document.f1.txt1.value==4){ 
+    document.f1.txt3.value="හතර ";
+    
+            document.f2.clip5.style.left=m4;
+               m4-=2;
+               if(m4>480)
+                     window.setTimeout("start();", 0);
+    document.f3.clip1.src="/assets/images/4.gif";
+        }
+    if(document.f1.txt1.value==5){ 
+    document.f1.txt3.value="පහ ";
+    
+            document.f2.clip6.style.left=m5;
+               m5-=2;
+               if(m5>530)
+                     window.setTimeout("start();", 0);
+    document.f3.clip1.src="/assets/images/5.gif";
+        }
+    
+    }
+    
+    
