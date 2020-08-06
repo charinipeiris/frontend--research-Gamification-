69ab803123ddf18e1811f64b6b956f33c0024802
charinipeiris <30485000+charinipeiris@users.noreply.github.com>
2020-08-06 18:13:35 +0530

    length-game created

--- src/app/grade1/maths/level1/width1/width1.component.ts
+++ src/app/grade1/maths/level1/width1/width1.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+declare function testlengthgame1(): any;
+declare function testlengthgame2(): any;
+declare function testlengthgame3(): any;
+declare function testlengthgame4(): any;
+declare function testlengthgame5(): any;
+declare function testlengthgame6(): any;
 @Component({
   selector: 'app-width1',
   templateUrl: './width1.component.html',
@@ -10,6 +15,12 @@ export class Width1Component implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // testlengthgame1();
+    // testlengthgame2();
+    // testlengthgame3();
+    // testlengthgame4();
+    // testlengthgame5();
+    // testlengthgame6();
   }
 
 }
