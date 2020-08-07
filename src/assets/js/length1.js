1459924281842bf4534f6f5ae2492f4deb38beea
Dinithi96 <dinithi96@users.noreply.github.com>
2020-08-07 09:17:51 +0530

    Grade 1 - Maths - Length
    Added audio files; tall and short

--- src/assets/js/length1.js
+++ src/assets/js/length1.js
@@ -2,6 +2,8 @@
     function testlength1(){
     document.f1.txt1.value="මම උසයි";
     document.f1.txt2.value="";
+    var myAudio = new Audio('/assets/audio/tall.mp3');
+    myAudio.play();
     //document.f1.clip3.src="/assets/images/blank.png";
     // document.f1.clip. src="/assets/images/0.gif";
     }
@@ -11,6 +13,8 @@
     function testlength2(){
     document.f1.txt2.value="මම මිටියි";
     document.f1.txt1.value="";
+    var myAudio = new Audio('/assets/audio/short.mp3');
+    myAudio.play();
     //document.f1.clip.src="/assets/images/blank.png"; 
     //document.f1.clip3. src="/assets/images/0.gif";
     }
