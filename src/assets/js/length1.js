fe995f8d82b45630597012b7e52cc38ff205e9be
charinipeiris <30485000+charinipeiris@users.noreply.github.com>
2020-09-28 18:22:34 +0530

    chnages to length intro
    chnages to length intro

--- src/assets/js/length1.js
+++ src/assets/js/length1.js
@@ -1,134 +1,48 @@
-
-    function testlength1(){
+function testlength1(){
     document.f1.txt1.value="මම උසයි";
     document.f1.txt2.value="";
-    document.f1.txt3.value="";
-    document.f1.txt4.value="";
-    document.f1.txt5.value="";
-    document.f1.txt6.value="";
     var myAudio = new Audio('/assets/audio/tall.mp3');
     myAudio.play();
-    //document.f1.clip3.src="/assets/images/blank.png";
-    // document.f1.clip. src="/assets/images/0.gif";
     }
-    
-    
     
     function testlength2(){
     document.f1.txt2.value="මම මිටියි";
     document.f1.txt1.value="";
-    document.f1.txt3.value="";
-    document.f1.txt4.value="";
-    document.f1.txt5.value="";
-    document.f1.txt6.value="";
     var myAudio = new Audio('/assets/audio/short.mp3');
     myAudio.play();
-    //document.f1.clip.src="/assets/images/blank.png"; 
-    //document.f1.clip3. src="/assets/images/0.gif";
     }
+    // -----------------------------
 
-     
     function testlength3(){
         document.f1.txt3.value="මම දිගයි";
         document.f1.txt4.value="";
-        document.f1.txt1.value="";
-        document.f1.txt2.value="";
-        document.f1.txt5.value="";
-        document.f1.txt6.value="";
         var myAudio = new Audio('/assets/audio/long.mp3');
         myAudio.play();
-        //document.f1.clip.src="/assets/images/blank.png"; 
-        //document.f1.clip3. src="/assets/images/0.gif";
+    
         }
 
          
     function testlength4(){
         document.f1.txt4.value="මම කොටයි";
         document.f1.txt3.value="";
-        document.f1.txt2.value="";
-        document.f1.txt1.value="";
-        document.f1.txt5.value="";
-        document.f1.txt6.value="";
         var myAudio = new Audio('/assets/audio/not_long.mp3');
         myAudio.play();
-        //document.f1.clip.src="/assets/images/blank.png"; 
-        //document.f1.clip3. src="/assets/images/0.gif";
+      
         }
 
              
     function testlength5(){
         document.f1.txt5.value="මම ලොකුයි";
-        document.f1.txt3.value="";
-        document.f1.txt2.value="";
-        document.f1.txt1.value="";
         document.f1.txt6.value="";
-        document.f1.txt4.value="";
+      
         var myAudio = new Audio('/assets/audio/big.mp3');
         myAudio.play();
-        //document.f1.clip.src="/assets/images/blank.png"; 
-        //document.f1.clip3. src="/assets/images/0.gif";
+       
         }
              
     function testlength6(){
         document.f1.txt6.value="මම පොඩියි";
-        document.f1.txt3.value="";
-        document.f1.txt2.value="";
-        document.f1.txt1.value="";
         document.f1.txt5.value="";
-        document.f1.txt4.value="";
         var myAudio = new Audio('/assets/audio/small.mp3');
         myAudio.play();
-        //document.f1.clip.src="/assets/images/blank.png"; 
-        //document.f1.clip3. src="/assets/images/0.gif";
-        }
-
-// -------game----------------------------------
-
-
-    function testlengthgame1(){
-    document.f1.txt1.value="නැවතත්";
-    document.f1.clip2.src="/assets/images/blank.png";
-    //document.f1.clip1. src="/assets/images/line2.gif";
-    document.f1.clip7.src="/assets/images/w.gif";
     }
-    
-
-    function testlengthgame2(){
-    document.f1.txt1.value="හරියට හරි";
-    document.f1.clip1.src="/assets/images/blank.png"; 
-    //document.f1.clip2. src="/assets/images/line2.gif";
-    document.f1.clip7.src="/assets/images/r.gif";
-    }
-
-    
-    function testlengthgame3(){
-        document.f1.txt2.value="නැවතත්";
-        document.f1.clip4.src="/assets/images/blank.png";
-        //document.f1.clip3. src="/assets/images/line2.gif";
-        document.f1.clip8.src="/assets/images/w.gif";
-    }
-    function testlengthgame4(){
-        document.f1.txt2.value="හරියට හරි";
-        document.f1.clip3.src="/assets/images/blank.png"; 
-        //document.f1.clip4. src="/assets/images/line2.gif";
-        document.f1.clip8.src="/assets/images/r.gif";
-        }
-
-    function testlengthgame5(){
-            document.f1.txt3.value="හරියට හරි";
-            document.f1.clip6.src="/assets/images/blank.png"; 
-            //document.f1.clip5. src="/assets/images/line2.gif";
-            document.f1.clip9.src="/assets/images/w.gif";
-            }
-    
-  function testlengthgame6(){
-    document.f1.txt3.value="හරියට හරි";
-    document.f1.clip5.src="/assets/images/blank.png"; 
-    //document.f1.clip6. src="/assets/images/line2.gif";
-    document.f1.clip9.src="/assets/images/r.gif";
-    }
-   
-    
-  
-    
-    
