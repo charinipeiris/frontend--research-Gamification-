957b6f5b387460d3e99fafc552e77be4bb153e78
charinipeiris <30485000+charinipeiris@users.noreply.github.com>
2020-10-03 23:23:37 +0530

    redesigning length intro part 1
    redesigning length intro part 1

--- src/assets/js/length1.js
+++ src/assets/js/length1.js
@@ -1,24 +1,22 @@
-function testlength1(){
+   function testlength1(){
     document.f1.txt1.value="මම උසයි";
     document.f1.txt2.value="";
     var myAudio = new Audio('/assets/audio/tall.mp3');
     myAudio.play();
     }
-    
+
     function testlength2(){
     document.f1.txt2.value="මම මිටියි";
     document.f1.txt1.value="";
     var myAudio = new Audio('/assets/audio/short.mp3');
     myAudio.play();
     }
-    // -----------------------------
 
     function testlength3(){
         document.f1.txt3.value="මම දිගයි";
         document.f1.txt4.value="";
         var myAudio = new Audio('/assets/audio/long.mp3');
         myAudio.play();
-    
         }
 
          
@@ -27,17 +25,14 @@ function testlength1(){
         document.f1.txt3.value="";
         var myAudio = new Audio('/assets/audio/not_long.mp3');
         myAudio.play();
-      
-        }
+    }
 
              
     function testlength5(){
         document.f1.txt5.value="මම ලොකුයි";
         document.f1.txt6.value="";
-      
         var myAudio = new Audio('/assets/audio/big.mp3');
         myAudio.play();
-       
         }
              
     function testlength6(){
@@ -45,4 +40,8 @@ function testlength1(){
         document.f1.txt5.value="";
         var myAudio = new Audio('/assets/audio/small.mp3');
         myAudio.play();
-    }
+        }
+
+    
+// -------game----------------------------------
+
