2171988d8fec8c12bc246e3914463acf75455832
charinipeiris <30485000+charinipeiris@users.noreply.github.com>
2020-10-10 08:00:00 +0530

    changes to to length component part1

--- src/assets/js/length1.js
+++ src/assets/js/length1.js
@@ -45,54 +45,50 @@
     
 // -------game----------------------------------
 
-
-function test(){
+function testlgame(){
     document.f1.txt1.value="Sorry";
     document.f1.clip2.src="/assets/images/blank.png";
    // document.f1.clip1. src="/assets/images/line1.gif";
     document.f1.clip7.src="/assets/images/w.gif";
+    document.getElementById("lkid1").style.pointerEvents ="none";
     }
     
-    
-    
-    function test2(){
+    function testlgame2(){
     document.f1.txt1.value="Congrats!!";
-    
     document.f1.clip1.src="/assets/images/blank.png"; 
-    document.f1.clip2. src="/assets/images/wow.gif";
+    //document.f1.clip2. src="/assets/images/wow.gif";
     document.f1.clip7.src="/assets/images/Tress.gif";
+    document.getElementById("lkid2").style.pointerEvents ="none";
     }
     
-    
-    function test3(){
-    
+    function testlgame3(){ 
     document.f1.txt2.value="Congrats!!";
     document.f1.clip4.src="/assets/images/blank.png";
-    document.f1.clip3.src="/assets/images/wow.gif";
+    //document.f1.clip3.src="/assets/images/wow.gif";
     document.f1.clip8.src="/assets/images/Tress.gif";
+    document.getElementById("t1").style.pointerEvents ="none";
     }
     
-    function test4(){
+    function testlgame4(){
     document.f1.txt2.value="Sorry";
-    
     document.f1.clip3.src="/assets/images/blank.png"; 
    // document.f1.clip4. src="/assets/images/line1.gif";
     document.f1.clip8.src="/assets/images/w.gif";
+    document.getElementById("t2").style.pointerEvents ="none";
     }
     
-    function test5(){
+    function testlgame5(){
     document.f1.txt3.value="Sorry";
-    
     document.f1.clip6.src="/assets/images/blank.png";
     // document.f1.clip5. src="/assets/images/line1.gif";
     document.f1.clip9.src="/assets/images/w.gif";
+    document.getElementById("p1").style.pointerEvents ="none";
     }
     
-
-    function test6(){
+    function testlgame6(){
     document.f1.txt3.value="Congrats!!";
-    
     document.f1.clip5.src="/assets/images/blank.png"; 
-    document.f1.clip6. src="/assets/images/wow.gif";
+    //document.f1.clip6. src="/assets/images/wow.gif";
     document.f1.clip9.src="/assets/images/Tress.gif";
+    document.getElementById("p2").style.pointerEvents ="none";
     }
