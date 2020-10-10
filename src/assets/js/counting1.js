e8061aa90ac8d37128d7dcfc08aacbf07ce4c98c
charinipeiris <30485000+charinipeiris@users.noreply.github.com>
2020-10-10 06:55:07 +0530

    few changes to counting component

--- src/assets/js/counting1.js
+++ src/assets/js/counting1.js
@@ -4,7 +4,7 @@ var m2 = 1000;
 var m3 = 1000;
 var m4 = 1000;
 var m5 = 1000;
-function test(){document.f1.txt1.style.visibility='hidden';
+function testcountintro(){document.f1.txt1.style.visibility='hidden';
 document.f1.txt3.style.visibility='hidden';
 document.f3.clip1.style.visibility='hidden';
 a++;
@@ -78,4 +78,81 @@ document.f1.txt3.value="පහ ";
 document.f3.clip1.src="/assets/images/5.gif";
     }
 
+}
+
+
+function ov()
+{
+document.getElementById("box").style.background="transparent"
+}
+
+function out()
+{
+document.getElementById("box").style.background="transparent"
+}
+var x =0
+function testcount1()
+{
+x++
+document.f1.t2.value = x;
+if (x>=10)
+{
+x=0;
+}
+if(x==1)
+document.f1.clip1.src="/assets/images/monky.png";
+if(x==2)
+document.f1.clip2.src="/assets/images/monky.png";
+if(x==3)
+document.f1.clip3.src="/assets/images/monky.png";
+if(x==4)
+document.f1.clip4.src="/assets/images/monky.png";
+if(x==5)
+document.f1.clip5.src="/assets/images/monky.png";
+if(x==6)
+document.f1.clip6.src="/assets/images/monky.png";
+if(x==7)
+document.f1.clip7.src="/assets/images/monky.png";
+if(x==8)
+document.f1.clip8.src="/assets/images/monky.png";
+if(x==9)
+document.f1.clip9.src="/assets/images/monky.png";
+if(x==0){
+document.f1.clip1.src="/assets/images/blank.png";
+document.f1.clip2.src="/assets/images/blank.png";
+document.f1.clip3.src="/assets/images/blank.png";
+document.f1.clip4.src="/assets/images/blank.png";
+document.f1.clip5.src="/assets/images/blank.png";
+document.f1.clip6.src="/assets/images/blank.png";
+document.f1.clip7.src="/assets/images/blank.png";
+document.f1.clip8.src="/assets/images/blank.png";
+document.f1.clip9.src="/assets/images/blank.png";}
+}
+
+function testcount2()
+{
+if(document.f1.t1.value==document.f1.t2.value)
+{document.f1.clip10.src="/assets/images/r.gif";
+// document.f1.clip11.src="/assets/images/c.gif"
+}else{
+document.f1.clip10.src="/assets/images/w.gif";
+document.f1.clip11.src="/assets/images/blank.png"
+}
+}
+
+function testcount3()
+{
+x=0
+document.f1.t1.value=1+Math.floor(Math.random()*9);
+document.f1.clip1.src="/assets/images/blank.png";
+document.f1.clip2.src="/assets/images/blank.png";
+document.f1.clip3.src="/assets/images/blank.png";
+document.f1.clip4.src="/assets/images/blank.png";
+document.f1.clip5.src="/assets/images/blank.png";
+document.f1.clip6.src="/assets/images/blank.png";
+document.f1.clip7.src="/assets/images/blank.png";
+document.f1.clip8.src="/assets/images/blank.png";
+document.f1.clip9.src="/assets/images/blank.png";
+document.f1.clip10.src="/assets/images/blank.png";
+document.f1.clip11.src="/assets/images/blank.png"
 }
