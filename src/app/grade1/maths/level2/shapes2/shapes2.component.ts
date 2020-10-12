b4b427ea864e6ef38e72a15dfbc483c052e018c6
Hirasha Pooliyadda <37568672+Hirasha@users.noreply.github.com>
2020-10-12 16:53:30 +0530

    completed maths grade 1 - llevel 2 shapes games

--- src/app/grade1/maths/level2/shapes2/shapes2.component.ts
+++ src/app/grade1/maths/level2/shapes2/shapes2.component.ts
@@ -12,4 +12,204 @@ export class Shapes2Component implements OnInit {
   ngOnInit(): void {
   }
 
+
+
+markstot(){
+  var total = parseInt(document.getElementById("marksArea").innerHTML)
+
+ 
+    if (total == 8){
+
+  var modal = document.getElementById("myModal");
+  var subbtn = document.getElementById("final_button");
+  modal.style.display = "block";
+  subbtn.style.display = "none";
+  setTimeout(function(){
+    document.getElementById('coin1').style.visibility = "visible";
+    },1000);
+  
+  setTimeout(function(){
+    document.getElementById('coin2').style.visibility = "visible";
+    },1500);
+  
+  setTimeout(function(){
+      document.getElementById('coin3').style.visibility = "visible";
+      },2000);
+  
+  setTimeout(function(){
+        document.getElementById('coin4').style.visibility = "visible";
+        },2200);
+  
+  
+  }
+  else if (total == 7){
+    var modal = document.getElementById("myModal");
+    var subbtn = document.getElementById("final_button");
+    modal.style.display = "block";
+    subbtn.style.display = "none";
+    setTimeout(function(){
+      document.getElementById('coin1').style.visibility = "visible";
+      },1000);
+    
+    setTimeout(function(){
+      document.getElementById('coin2').style.visibility = "visible";
+      },1500);
+    
+    setTimeout(function(){
+        document.getElementById('coin3').style.visibility = "visible";
+        },2000);
+    
+    setTimeout(function(){
+          document.getElementById('coin4').style.visibility = "visible";
+          },2200);
+  }
+  
+  else if(total == 6){
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+    setTimeout(function(){
+      document.getElementById('coin1').style.visibility = "visible";
+      },1000);
+    
+      setTimeout(function(){
+        document.getElementById('coin2').style.visibility = "visible";
+        },1000);
+      
+     setTimeout(function(){
+          document.getElementById('coin3').style.visibility = "visible";
+          },1000);
+
+     setTimeout(function(){
+         document.getElementById('coin4').style.visibility = "visible";
+            },1000);
+     document.getElementById('coin4').style.opacity ="0.3";
+      
+    
+  }
+  else if(total == 5){
+    var modal = document.getElementById("myModal");
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+      setTimeout(function(){
+        document.getElementById('coin1').style.visibility = "visible";
+        },1000);
+      
+        setTimeout(function(){
+          document.getElementById('coin2').style.visibility = "visible";
+          },1000);
+        
+       setTimeout(function(){
+            document.getElementById('coin3').style.visibility = "visible";
+            },1000);
+  
+       setTimeout(function(){
+           document.getElementById('coin4').style.visibility = "visible";
+              },1000);
+       document.getElementById('coin4').style.opacity ="0.3";
+    }
+  
+    else if (total==4) {
+      var modal = document.getElementById("myModal");
+      modal.style.display = "block";
+        setTimeout(function(){
+          document.getElementById('coin1').style.visibility = "visible";
+          },1000);
+        
+          setTimeout(function(){
+            document.getElementById('coin2').style.visibility = "visible";
+            },1000);
+          
+         setTimeout(function(){
+              document.getElementById('coin3').style.visibility = "visible";
+              },1000);
+        document.getElementById('coin3').style.opacity ="0.3";
+    
+         setTimeout(function(){
+             document.getElementById('coin4').style.visibility = "visible";
+                },1000);
+         document.getElementById('coin4').style.opacity ="0.3";
+    }
+  
+    else if (total == 3){
+      var modal = document.getElementById("myModal");
+      modal.style.display = "block";
+        setTimeout(function(){
+          document.getElementById('coin1').style.visibility = "visible";
+          },1000);
+        
+          setTimeout(function(){
+            document.getElementById('coin2').style.visibility = "visible";
+            },1000);
+          
+         setTimeout(function(){
+              document.getElementById('coin3').style.visibility = "visible";
+              },1000);
+        document.getElementById('coin3').style.opacity ="0.3";
+    
+         setTimeout(function(){
+             document.getElementById('coin4').style.visibility = "visible";
+                },1000);
+         document.getElementById('coin4').style.opacity ="0.3";
+    }
+
+    else if (total == 2){
+      var modal = document.getElementById("myModal");
+      modal.style.display = "block";
+        setTimeout(function(){
+          document.getElementById('coin1').style.visibility = "visible";
+          },1000);
+        
+          setTimeout(function(){
+            document.getElementById('coin2').style.visibility = "visible";
+            },1000);
+            document.getElementById('coin2').style.opacity ="0.3";
+          
+         setTimeout(function(){
+              document.getElementById('coin3').style.visibility = "visible";
+              },1000);
+        document.getElementById('coin3').style.opacity ="0.3";
+    
+         setTimeout(function(){
+             document.getElementById('coin4').style.visibility = "visible";
+                },1000);
+         document.getElementById('coin4').style.opacity ="0.3";
+    }
+   
+    else if (total ==1){
+      var modal = document.getElementById("myModal");
+      modal.style.display = "block";
+        setTimeout(function(){
+          document.getElementById('coin1').style.visibility = "visible";
+          },1000);
+        
+          setTimeout(function(){
+            document.getElementById('coin2').style.visibility = "visible";
+            },1000);
+            document.getElementById('coin2').style.opacity ="0.3";
+          
+         setTimeout(function(){
+              document.getElementById('coin3').style.visibility = "visible";
+              },1000);
+        document.getElementById('coin3').style.opacity ="0.3";
+    
+         setTimeout(function(){
+             document.getElementById('coin4').style.visibility = "visible";
+                },1000);
+         document.getElementById('coin4').style.opacity ="0.3";
+      
+    }
+    else if(total == 0){
+      var modal = document.getElementById("myModal");
+      modal.style.display = "block";
+        setTimeout(function(){
+          document.getElementById('sad_coin').style.visibility = "visible";
+          },1000);
+    }
+    else{
+      
+    }
+  
+  
+  }
+  
 }
--- src/assets/js/shapes.js
+++ src/assets/js/shapes.js
@@ -1,7 +1,34 @@
-function show(Page1, Page2, Page3) {
-       document.getElementById(Page1).style.display='block';
-       document.getElementById(Page2).style.display='none'; 
-       document.getElementById(Page3).style.display='none'; 
+function show(Page1) {
+       var p1 = document.getElementById(Page1).getAttribute("id");
+       console.log(p1);
+       if (p1 == "Page1"){
+              document.getElementById("Page1").style.display='block';
+              document.getElementById("Page2").style.display='none'; 
+              document.getElementById("Page3").style.display='none'; 
+              document.getElementById("Page4").style.display='none'; 
+       }
+       else if (p1 == "Page2"){
+              document.getElementById("Page1").style.display='none';
+              document.getElementById("Page2").style.display='block'; 
+              document.getElementById("Page3").style.display='none'; 
+              document.getElementById("Page4").style.display='none'; 
+       }
+       else if (p1 == "Page3"){
+              document.getElementById("Page1").style.display='none';
+              document.getElementById("Page2").style.display='none'; 
+              document.getElementById("Page3").style.display='block'; 
+              document.getElementById("Page4").style.display='none'; 
+       }
+       else if (p1 == "Page4"){
+              document.getElementById("Page1").style.display='none';
+              document.getElementById("Page2").style.display='none'; 
+              document.getElementById("Page3").style.display='none'; 
+              document.getElementById("Page4").style.display='block'; 
+
+       }
+       else{
+
+       }
      }
 
 function play_intro(){
@@ -36,226 +63,270 @@ function play_c(){
 
 
 //Game - Circle
-$(document).ready(function(){
-       $(document).on('click',"#img2-1",function () {
-       var audio = document.getElementById("kids_say_yay");
-       if (audio.paused) audio.play();
-       else audio.pause();
-       $( "#popUp_right" ).show(); 
-              setTimeout(function() {
-              $( "#popUp_right" ).hide();
-                     }, 2000);
-});
-});
 
-$(document).ready(function(){
-       $(document).on('click',"#img3-1",function () {
-       var audio = document.getElementById("kids_say_yay");
-       if (audio.paused) audio.play();
-       else audio.pause();
-       $( "#popUp_right" ).show(); 
-              setTimeout(function() {
-              $( "#popUp_right" ).hide();
-                     }, 2000);
-});
-});
+// $(document).ready(function(){
+//        $(document).on('click',"#img2-1",function () {
+//        var audio = document.getElementById("kids_say_yay");
+//        if (audio.paused) audio.play();
+//        else audio.pause();
+//        $( "#popUp_right" ).show(); 
+//               setTimeout(function() {
+//               $( "#popUp_right" ).hide();
+//                      }, 2000);
+// });
+// });
+var total = 0;
+function loadAnswer(imgid)
+{
 
-$(document).ready(function(){
-       $(document).on('click',"#img4-1",function () {
-       $( "#popUp_right" ).show(); 
-       var audio = document.getElementById("kids_say_yay");
-       if (audio.paused) audio.play();
-       else audio.pause();
-              setTimeout(function() {
-              $( "#popUp_right" ).hide();
-                     }, 2000);
-});
-});
+       var correct1 = "http://localhost:4200/assets/images/shape_sun.png";
+       var correct2 = "http://localhost:4200/assets/images/shape_tree.png";
+       var correct3 = "http://localhost:4200/assets/images/shape_tire.png";
+       // console.log(imageLoading);
+       if( ((document.getElementById("answer1")).src) == "http://localhost:4200/assets/images/trans.svg" ){
+              (document.getElementById("answer1")).src = ((document.getElementById(imgid)).src);
+              document.getElementById(imgid).style.pointerEvents = "none";
+       }
+       else if ( ((document.getElementById("answer2")).src) == "http://localhost:4200/assets/images/trans.svg")
+       {
+              (document.getElementById("answer2")).src = ((document.getElementById(imgid)).src);
+              document.getElementById(imgid).style.pointerEvents = "none";
 
-$(document).ready(function(){
-       $(document).on('click',"#img5-1",function () {
-       $( "#popUp_right" ).show(); 
-       var audio = document.getElementById("kids_say_yay");
-       if (audio.paused) audio.play();
-       else audio.pause();
-              setTimeout(function() {
-              $( "#popUp_right" ).hide();
-                     }, 2000);
-});
-});
+              var image1 = ((document.getElementById("answer1")).src);
+              var image2 = ((document.getElementById("answer2")).src)
+              if (image1 == correct1){
+                     total = total + 1;
+              }
+              else if (image1 == correct2){
+                     total = total + 1;
+              }
+              else if (image1 == correct3){
+                     total= total + 1; 
+              }
+              else{
+                     //no mark for first answer
+              }
 
-$(document).ready(function(){
-       $(document).on('click',"#img6-1",function () {
-       $( "#popUp_wrong" ).show(); 
-       var audio = document.getElementById("kids_say_ohno");
-       if (audio.paused) audio.play();
-       else audio.pause();
-              setTimeout(function() {
-              $( "#popUp_wrong" ).hide();
-                     }, 2000);
-});
-});
+               if (image2 == correct1){
+                     total = total + 1;
+              }
+              else if (image2 == correct2){
+                     total = total + 1;
+              }
+              else if (image2 == correct3){
+                     total= total + 1; 
+              }
+              else{
+                     //no mark for second answer
+              }
+              document.getElementById("next1").style.visibility= "visible"; //to enable "NEXT" button
+              marksArea.innerHTML = total;
+              // document.getElementById(total).style.visibility="visible";
+              for (i = 0; i < 9; i++) {
+                    if( (document.getElementById(i).getAttribute("id")) == total){
+                            document.getElementById(i).style.visibility="visible";
+                    }
+                    else{
+                     document.getElementById(i).style.visibility="hidden";
+                    }
+
+                   }
+       }
+       else{
+
+              // console.log("magula3");
+              // document.getElementById("error").style.visibility= "visible";
+       }
 
 
-$(document).ready(function(){
-       $(document).on('click',"#img7-1",function () {
-       $( "#popUp_wrong" ).show(); 
-       var audio = document.getElementById("kids_say_ohno");
-       if (audio.paused) audio.play();
-       else audio.pause();
-              setTimeout(function() {
-              $( "#popUp_wrong" ).hide();
-                     }, 2000);
-});
-});
-
-$(document).ready(function(){
-       $(document).on('click',"#img8-1",function () {
-       $( "#popUp_wrong" ).show(); 
-       var audio = document.getElementById("kids_say_ohno");
-       if (audio.paused) audio.play();
-       else audio.pause();
-              setTimeout(function() {
-              $( "#popUp_wrong" ).hide();
-                     }, 2000);
-});
-});
-
-$(document).ready(function(){
-       $(document).on('click',"#img9-1",function () {
-       $( "#popUp_wrong" ).show(); 
-       var audio = document.getElementById("kids_say_ohno");
-       if (audio.paused) audio.play();
-       else audio.pause();
-              setTimeout(function() {
-              $( "#popUp_wrong" ).hide();
-                     }, 2000);
-});
-});
-
-$(document).ready(function(){
-       $(document).on('click',"#img10-1",function () {
-       $( "#popUp_wrong" ).show(); 
-       var audio = document.getElementById("kids_say_ohno");
-       if (audio.paused) audio.play();
-       else audio.pause();
-              setTimeout(function() {
-              $( "#popUp_wrong" ).hide();
-                     }, 2000);
-});
-});
+}
 
 
 
-//Game - Triangle
-$(document).ready(function(){
-       $(document).on('click',"#img2-2",function () {
-       var audio = document.getElementById("kids_say_ohno");
-       if (audio.paused) audio.play();
-       else audio.pause();
-       $( "#popUp_wrong1" ).show(); 
-              setTimeout(function() {
-              $( "#popUp_wrong1" ).hide();
-                     }, 2000);
-});
-});
+function loadAnswer2(imgid)
+{
 
-$(document).ready(function(){
-       $(document).on('click',"#img3-2",function () {
-       var audio = document.getElementById("kids_say_ohno");
-       if (audio.paused) audio.play();
-       else audio.pause();
-       $( "#popUp_wrong1" ).show(); 
-              setTimeout(function() {
-              $( "#popUp_wrong1" ).hide();
-                     }, 2000);
-});
-});
+       var correct3 = "http://localhost:4200/assets/images/shape_triangle.png";
+       var correct4 = "http://localhost:4200/assets/images/shape_triangle_1.png";
 
-$(document).ready(function(){
-       $(document).on('click',"#img4-2",function () {
-       $( "#popUp_wrong1" ).show(); 
-       var audio = document.getElementById("kids_say_ohno");
-       if (audio.paused) audio.play();
-       else audio.pause();
-              setTimeout(function() {
-              $( "#popUp_wrong1" ).hide();
-                     }, 2000);
-});
-});
+       // console.log(imageLoading);
+       if( ((document.getElementById("answer3")).src) == "http://localhost:4200/assets/images/trans.svg" ){
+              (document.getElementById("answer3")).src = ((document.getElementById(imgid)).src);
+              document.getElementById(imgid).style.pointerEvents = "none";
+       }
+       else if ( ((document.getElementById("answer4")).src) == "http://localhost:4200/assets/images/trans.svg")
+       {
+              (document.getElementById("answer4")).src = ((document.getElementById(imgid)).src);
+              document.getElementById(imgid).style.pointerEvents = "none";
 
-$(document).ready(function(){
-       $(document).on('click',"#img5-2",function () {
-       $( "#popUp_wrong1" ).show(); 
-       var audio = document.getElementById("kids_say_ohno");
-       if (audio.paused) audio.play();
-       else audio.pause();
-              setTimeout(function() {
-              $( "#popUp_wrong1" ).hide();
-                     }, 2000);
-});
-});
+              var image1 = ((document.getElementById("answer3")).src);
+              var image2 = ((document.getElementById("answer4")).src)
+              var sub_total = parseInt(document.getElementById("marksArea").innerHTML);
+              if (image1 == correct3){
+                     sub_total = sub_total + 1;
+              }
+              else if (image1 == correct4){
+                     sub_total = sub_total + 1;
+              }
+              else{
+                     //no mark for first answer
+              }
 
-$(document).ready(function(){
-       $(document).on('click',"#img6-2",function () {
-       $( "#popUp_right1" ).show(); 
-       var audio = document.getElementById("kids_say_yay");
-       if (audio.paused) audio.play();
-       else audio.pause();
-              setTimeout(function() {
-              $( "#popUp_right1" ).hide();
-                     }, 2000);
-});
-});
+               if (image2 == correct3){
+                     sub_total = sub_total + 1;
+              }
+              else if (image2 == correct4){
+                     sub_total = sub_total + 1;
+              }
+              else{
+                     //no mark for second answer
+              }
+              document.getElementById("next2").style.visibility= "visible"; //to enable "NEXT" button
+              marksArea.innerHTML = sub_total;
+              // document.getElementById(sub_total).style.visibility="visible";
+              for (i = 0; i < 9; i++) {
+                     if( (document.getElementById(i).getAttribute("id")) == sub_total){
+                             document.getElementById(i).style.visibility="visible";
+                     }
+                     else{
+                      document.getElementById(i).style.visibility="hidden";
+                     }
+ 
+                    }
+       }
+       else{
+
+              // console.log("magula3");
+              // document.getElementById("error").style.visibility= "visible";
+       }
 
 
-$(document).ready(function(){
-       $(document).on('click',"#img7-2",function () {
-       $( "#popUp_right1" ).show(); 
-       var audio = document.getElementById("kids_say_yay");
-       if (audio.paused) audio.play();
-       else audio.pause();
-              setTimeout(function() {
-              $( "#popUp_right1" ).hide();
-                     }, 2000);
-});
-});
+}
 
-$(document).ready(function(){
-       $(document).on('click',"#img8-2",function () {
-       $( "#popUp_wrong1" ).show(); 
-       var audio = document.getElementById("kids_say_ohno");
-       if (audio.paused) audio.play();
-       else audio.pause();
-              setTimeout(function() {
-              $( "#popUp_wrong1" ).hide();
-                     }, 2000);
-});
-});
+function loadAnswer3(imgid)
+{
 
-$(document).ready(function(){
-       $(document).on('click',"#img9-2",function () {
-       $( "#popUp_wrong1" ).show(); 
-       var audio = document.getElementById("kids_say_ohno");
-       if (audio.paused) audio.play();
-       else audio.pause();
-              setTimeout(function() {
-              $( "#popUp_wrong1" ).hide();
-                     }, 2000);
-});
-});
+       var correct5 = "http://localhost:4200/assets/images/shape_square.png";
+       var correct6 = "http://localhost:4200/assets/images/shape_square_1.png";
 
-$(document).ready(function(){
-       $(document).on('click',"#img10-2",function () {
-       $( "#popUp_wrong1" ).show(); 
-       var audio = document.getElementById("kids_say_ohno");
-       if (audio.paused) audio.play();
-       else audio.pause();
-              setTimeout(function() {
-              $( "#popUp_wrong1" ).hide();
-                     }, 2000);
-});
-});
+       // console.log(imageLoading);
+       if( ((document.getElementById("answer5")).src) == "http://localhost:4200/assets/images/trans.svg" ){
+              (document.getElementById("answer5")).src = ((document.getElementById(imgid)).src);
+              document.getElementById(imgid).style.pointerEvents = "none";
+       }
+       else if ( ((document.getElementById("answer6")).src) == "http://localhost:4200/assets/images/trans.svg")
+       {
+              (document.getElementById("answer6")).src = ((document.getElementById(imgid)).src);
+              document.getElementById(imgid).style.pointerEvents = "none";
+
+              var image1 = ((document.getElementById("answer5")).src);
+              var image2 = ((document.getElementById("answer6")).src)
+              var sub_total = parseInt(document.getElementById("marksArea").innerHTML);
+              if (image1 == correct5){
+                     sub_total = sub_total + 1;
+              }
+              else if (image1 == correct6){
+                     sub_total = sub_total + 1;
+              }
+              else{
+                     //no mark for first answer
+              }
+
+               if (image2 == correct5){
+                     sub_total = sub_total + 1;
+              }
+              else if (image2 == correct6){
+                     sub_total = sub_total + 1;
+              }
+              else{
+                     //no mark for second answer
+              }
+              document.getElementById("next3").style.visibility= "visible"; //to enable "NEXT" button
+              marksArea.innerHTML = sub_total;
+              // document.getElementById(sub_total).style.visibility="visible";
+              for (i = 0; i < 9; i++) {
+                     if( (document.getElementById(i).getAttribute("id")) == sub_total){
+                             document.getElementById(i).style.visibility="visible";
+                     }
+                     else{
+                      document.getElementById(i).style.visibility="hidden";
+                     }
+ 
+                    }
+       }
+       else{
+
+              // console.log("magula3");
+              // document.getElementById("error").style.visibility= "visible";
+       }
+
+
+}
+
+
+function loadAnswer4(imgid)
+{
+
+       var correct7 = "http://localhost:4200/assets/images/shape_rect.png";
+       var correct8 = "http://localhost:4200/assets/images/shape_rect1.png";
+
+       // console.log(imageLoading);
+       if( ((document.getElementById("answer7")).src) == "http://localhost:4200/assets/images/trans.svg" ){
+              (document.getElementById("answer7")).src = ((document.getElementById(imgid)).src);
+              document.getElementById(imgid).style.pointerEvents = "none";
+       }
+       else if ( ((document.getElementById("answer8")).src) == "http://localhost:4200/assets/images/trans.svg")
+       {
+              (document.getElementById("answer8")).src = ((document.getElementById(imgid)).src);
+              document.getElementById(imgid).style.pointerEvents = "none";
+
+              var image1 = ((document.getElementById("answer7")).src);
+              var image2 = ((document.getElementById("answer8")).src)
+              var sub_total = parseInt(document.getElementById("marksArea").innerHTML);
+              if (image1 == correct7){
+                     sub_total = sub_total + 1;
+              }
+              else if (image1 == correct8){
+                     sub_total = sub_total + 1;
+              }
+              else{
+                     //no mark for first answer
+              }
+
+               if (image2 == correct7){
+                     sub_total = sub_total + 1;
+              }
+              else if (image2 == correct8){
+                     sub_total = sub_total + 1;
+              }
+              else{
+                     //no mark for second answer
+              }
+              document.getElementById("final_button").style.visibility= "visible";
+              marksArea.innerHTML = sub_total;
+              // document.getElementById(sub_total).style.visibility="visible";
+              for (i = 0; i < 9; i++) {
+                     if( (document.getElementById(i).getAttribute("id")) == sub_total){
+                             document.getElementById(i).style.visibility="visible";
+                     }
+                     else{
+                      document.getElementById(i).style.visibility="hidden";
+                     }
+ 
+                    }
+       }
+       else{
+
+              // console.log("magula3");
+              // document.getElementById("error").style.visibility= "visible";
+       }
+
+
+}
+
+
+
+
 
 $(document).ready(function(){
        $(document).on('click',"#question-circle",function () {
@@ -272,6 +343,24 @@ $(document).ready(function(){
        else audio.pause();
    });
 });
+
+$(document).ready(function(){
+       $(document).on('click',"#question-square",function () {
+       var audio = document.getElementById("quest_audio3");
+       if (audio.paused) audio.play();
+       else audio.pause();
+   });
+});
+
+$(document).ready(function(){
+       $(document).on('click',"#question-rect",function () {
+       var audio = document.getElementById("quest_audio4");
+       if (audio.paused) audio.play();
+       else audio.pause();
+   });
+});
+
+
 
 
 
