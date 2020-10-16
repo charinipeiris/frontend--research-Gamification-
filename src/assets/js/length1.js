9fadf818b6eb557b898d7a1183115333791ea844
Hirasha Pooliyadda <37568672+Hirasha@users.noreply.github.com>
2020-10-16 11:15:46 +0530

    fixed the error in G1 L1 length game

--- src/assets/js/length1.js
+++ src/assets/js/length1.js
@@ -71,7 +71,7 @@
 
 function testlgame(){
     // document.f1.txt1.value="Sorry";
-    document.f1.clip2.src="/assets/images/blank.png";
+    // document.f1.clip2.src="/assets/images/blank.png";
     document.f1.clip7.src="/assets/images/w.gif";
     document.getElementById("lkid1").style.pointerEvents ="none";
     document.getElementById("unmark1").checked = true; //kid gave wrong answer 
@@ -80,7 +80,7 @@ function testlgame(){
     
     function testlgame2(){
     // document.f1.txt1.value="Congrats!!";
-    document.f1.clip1.src="/assets/images/blank.png"; 
+    // document.f1.clip1.src="/assets/images/blank.png"; 
     //document.f1.clip2. src="/assets/images/wow.gif";
     document.f1.clip7.src="/assets/images/yay_bunny2.gif";
     document.getElementById("lkid2").style.pointerEvents ="none";
@@ -90,7 +90,7 @@ function testlgame(){
     
     function testlgame3(){ 
     // document.f1.txt2.value="Congrats!!";
-    document.f1.clip4.src="/assets/images/blank.png";
+    // document.f1.clip4.src="/assets/images/blank.png";
     //document.f1.clip3.src="/assets/images/wow.gif";
     document.f1.clip8.src="/assets/images/yay_bunny2.gif";
     document.getElementById("tree2").style.pointerEvents ="none";
@@ -100,7 +100,7 @@ function testlgame(){
     
     function testlgame4(){
     // document.f1.txt2.value="Sorry";
-    document.f1.clip3.src="/assets/images/blank.png"; 
+    // document.f1.clip3.src="/assets/images/blank.png"; 
     document.f1.clip8.src="/assets/images/w.gif";
     document.getElementById("tree1").style.pointerEvents ="none";
     document.getElementById("unmark2").checked = true; //kid gave wrong answer 
@@ -109,7 +109,7 @@ function testlgame(){
     
     function testlgame5(){
     // document.f1.txt3.value="Sorry";
-    document.f1.clip6.src="/assets/images/blank.png";
+    // document.f1.clip6.src="/assets/images/blank.png";
     document.f1.clip9.src="/assets/images/w.gif";
     document.getElementById("pen2").style.pointerEvents ="none";
     document.getElementById("unmark3").checked = true; //kid gave wrong answer 
@@ -118,7 +118,7 @@ function testlgame(){
     
     function testlgame6(){
     // document.f1.txt3.value="Congrats!!";
-    document.f1.clip5.src="/assets/images/blank.png"; 
+    // document.f1.clip5.src="/assets/images/blank.png"; 
     //document.f1.clip6. src="/assets/images/wow.gif";
     document.f1.clip9.src="/assets/images/yay_bunny2.gif";
     document.getElementById("pen1").style.pointerEvents ="none";
