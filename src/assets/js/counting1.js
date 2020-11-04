ea9a426bc723fad2a307b52a5cafcf14680af50c
Dinithi96 <dinithi96@users.noreply.github.com>
2020-11-04 17:37:10 +0530

    Counting - Added 5 buttons
    G1-L1-Counting
    JS does not work

--- src/assets/js/counting1.js
+++ src/assets/js/counting1.js
@@ -22,89 +22,31 @@ function showpages_count_intro(page){
 }
 
 //-----------counting intro(1-5) level 1 grade 1---------------
-var a =0;
-var m1 = 1000;
-var m2 = 1000;
-var m3 = 1000;
-var m4 = 1000;
-var m5 = 1000;
-function testcountintro(){document.f1.txt1.style.visibility='hidden';
-document.f1.txt3.style.visibility='hidden';
-document.f3.clip1.style.visibility='hidden';
-a++;
-document.f1.txt1.value=a;
-if(a==6){document.f1.txt1.value=""; a="0";document.f.but1.disabled="disabled"; 
-window.setTimeout("start();", 0);
-document.f2.clip2.style.left=1000;
-document.f2.clip3.style.left=1000; 
-document.f2.clip4.style.left=1000;
-document.f2.clip5.style.left=1000;
-document.f2.clip6.style.left=1000;
-document.f1.txt3.value=" "; document.f3.clip1.src="/assets/images/b.png"; }
 
-    if(document.f1.txt1.value<=5 && document.f1.txt1.value>0){
-        document.f.but1.disabled="disabled";
-        start();
-        setTimeout("document.f.but1.disabled=false,document.f1.txt1.style.visibility='visible',document.f1.txt3.style.visibility='visible',document.f3.clip1.style.visibility='visible'",2500);
-        
-    }
-        
+function testcountintro1(){
+    document.f1.txt1.value="1";
+    document.f2.clip1.src="/assets/images/yay_bunny.gif";
 }
 
-function start(){
-    if(document.f1.txt1.value==1){ 
-document.f1.txt3.value='එක ';
-
-    document.f2.clip2.style.left=m1;
-       m1-=2;}
-       if(m1>320){
-             window.setTimeout("start();", 0);
-document.f3.clip1.src="/assets/images/1.gif";
-}
-                
-
-    if(document.f1.txt1.value==2){ 
-document.f1.txt3.value="දෙක ";
-
-        document.f2.clip3.style.left=m2;
-           m2-=2;
-           if(m2>370)
-                 window.setTimeout("start();", 0);
-document.f3.clip1.src="/assets/images/2.gif";
-    }
-
-    if(document.f1.txt1.value==3){ 
-document.f1.txt3.value="තුන ";
-
-        document.f2.clip4.style.left=m3;
-           m3-=2;
-           if(m3>430)
-                 window.setTimeout("start();", 0);
-document.f3.clip1.src="/assets/images/3.gif";
-    }
-
-if(document.f1.txt1.value==4){ 
-document.f1.txt3.value="හතර ";
-
-        document.f2.clip5.style.left=m4;
-           m4-=2;
-           if(m4>480)
-                 window.setTimeout("start();", 0);
-document.f3.clip1.src="/assets/images/4.gif";
-    }
-if(document.f1.txt1.value==5){ 
-document.f1.txt3.value="පහ ";
-
-        document.f2.clip6.style.left=m5;
-           m5-=2;
-           if(m5>530)
-                 window.setTimeout("start();", 0);
-document.f3.clip1.src="/assets/images/5.gif";
-    }
-
+function testcountintro2(){
+    document.f1.txt2.value="2";
+    document.f2.clip2.src="/assets/images/2.gif";
 }
 
+function testcountintro3(){
+    document.f1.txt3.value="3";
+    document.f2.clip3.src="/assets/images/3.gif";
+}
 
+function testcountintro4(){
+    document.f1.txt4.value="4";
+    document.f2.clip4.src="/assets/images/4.gif";
+}
+
+function testcountintro5(){
+    document.f1.txt5.value="5";
+    document.f2.clip5.src="/assets/images/5.gif";
+}
 //---------------- counting game - garade 1 level 1--------------//
 
 function testcounts1game(){
