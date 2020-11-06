1adc0ac3929d7ec445b88c95f7bae729af4a546a
Hirasha Pooliyadda <37568672+Hirasha@users.noreply.github.com>
2020-11-07 02:19:05 +0530

    added mouse clicking G2 L1 shapes

--- src/app/grade2/maths2/m-level1/shapes-l1/shapes-l1.component.ts
+++ src/app/grade2/maths2/m-level1/shapes-l1/shapes-l1.component.ts
@@ -21,8 +21,9 @@ export class ShapesL1Component implements OnInit {
 markstot_shapes2(){
 
 var time = document.getElementById("display").innerHTML;
-console.log(time);
 document.getElementById("time-back").style.visibility = "hidden";
+var clicksarray = document.getElementById("clickarray").innerHTML ;
+// console.log(clicksarray);
 
 var total = 0;
 
@@ -30,32 +31,16 @@ let element1 = <HTMLInputElement> document.getElementById("mark1");
 let element2 = <HTMLInputElement> document.getElementById("mark2");  
 let element3 = <HTMLInputElement> document.getElementById("mark3");  
 
-let element4 = <HTMLInputElement> document.getElementById("unmark1");  
-let element5 = <HTMLInputElement> document.getElementById("unmark2");  
-let element6 = <HTMLInputElement> document.getElementById("unmark3");  
-
 if  (element1.checked ){
   total = total + 1;
  }
-// else if ((!element1.checked) && (!element4.checked)){
-//   // document.getElementById('alert1').style.visibility = "visible";
-// }
 
 if (element2.checked)
     total = total + 1;
 
-// else if  ((!element2.checked) && (!element5.checked)){
-//   // document.getElementById('alert2').style.visibility = "visible";
-
-// }
 if (element3.checked)
     total = total + 1;
 
-// else if  ((!element3.checked) && (!element6.checked)){
-//   // document.getElementById('alert3').style.visibility = "visible";
-// }
-
-console.log(total);
 
 
     // let username = localStorage.getItem('uname');
@@ -65,7 +50,8 @@ console.log(total);
     //   game_id : "m16",
     //   marks : total,
     //   time_spent : time,
-    //   emotions: this.child.emotions
+    //   emotions: this.child.emotions,
+    //   max_idle_time :  clicksarray
     //   }
     // ]},username).subscribe(res=>{console.log("success")}, err=>{console.log("error")});
 
