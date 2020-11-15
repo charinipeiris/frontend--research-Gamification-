f768ba3231a583d7c9ebaabb0cb15b345aa89053
Hirasha Pooliyadda <37568672+Hirasha@users.noreply.github.com>
2020-11-15 13:03:00 +0530

    level disabling in sinhala completed

--- src/app/grade1/sinhala/sinhala-level1/sinhala-level1.component.ts
+++ src/app/grade1/sinhala/sinhala-level1/sinhala-level1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GamesService } from 'src/app/services/games.service';
 
 @Component({
   selector: 'app-sinhala-level1',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SinhalaLevel1Component implements OnInit {
 
-  constructor() { }
+  GamesArray = [];
+  btn1: number;
+  btn2: number;
+  btn3: number;
+  btn4: number;
+  nxtBtn : number;
+  levelGames1 = [];
+  constructor(private gameService: GamesService) { 
+    this.btn1 = 0;
+    this.btn2 = 0;
+    this.btn3 = 0;
+    this.btn4 = 0;
+    this.nxtBtn = 0;
+    this.levelGames1 =["S11","S12", "S13", "S14"];
+
+  }
 
   ngOnInit(): void {
+    var username = localStorage.getItem("uname");
+    this.gameService.getGamesforNav(username).subscribe((data:any[]) => {
+      this.GamesArray = data;
+
+        if (this.GamesArray.indexOf("S12") !== -1){
+            this.btn1 = this.btn1 + 1
+          }
+        if (this.GamesArray.indexOf("S11") !== -1){
+          this.btn2 = this.btn2 + 1
+          }
+        if (this.GamesArray.indexOf("S13") !== -1){
+        this.btn3 = this.btn3 + 1
+          }
+        if (this.GamesArray.indexOf("S14") !== -1){
+          this.btn4 = this.btn4 + 1
+          }
+
+          for (var i in this.GamesArray){
+            for (var j in this.levelGames1) {
+               // console.log(this.levelGames[j]); 
+               if((this.levelGames1[j])==(this.GamesArray[i])) {
+                //  console.log(this.GamesArray[i]);
+                 this.nxtBtn = this.nxtBtn + 1;
+               }
+             }
+           }
+
+    }, (error) => {
+      
+    });
   }
+
 
 }
