b49a8d88338af44d7547f782e578449cbaee6e79
Dinithi96 <dinithi96@users.noreply.github.com>
2021-01-09 19:09:26 +0530

    Audio files replaced in Grade 1 games

--- src/assets/js/sinhalaG1L1.js
+++ src/assets/js/sinhalaG1L1.js
@@ -357,12 +357,17 @@ function enableSubmit_sinhala(){
       document.getElementById("submit").style.display="none";
         }
         }
-//-----audio functions for word selection and riddle game-----
+//-----audio functions for Grade 1 level 1 game-----
   function audio_sinhala_wordselect() {
     var audio = document.getElementById("quest_audio_word-select");
     audio.play();
   }
 
+//audio function for Grade 1 level 2 game
+function audio_sinhala_wordselect_l2() {
+  var audio = document.getElementById("G1L2Sentences1");
+  audio.play();
+}
 
 //-----------------poems (Grade 1 level 1)-------------------------------
 
