045d85cc1a09c4fff52b8b2ead34a45189d20241
charinipeiris <30485000+charinipeiris@users.noreply.github.com>
2021-01-21 16:36:47 +0530

    grade 1 level1 env game 3 fully completed

--- src/assets/js/envL1.js
+++ src/assets/js/envL1.js
@@ -1,3 +1,4 @@
+//---------------game 1 ---------------------------
 
   function family1()
   {
@@ -163,3 +164,171 @@
         document.getElementById("submit").style.display="none";
           }
           }
+
+//-----------game 2------------------------------------
+
+
+function animal1()
+{
+document.getElementById("anim1").style.border = "4px solid blue"; 
+document.getElementById("anim2").style.pointerEvents = "none";
+document.getElementById("anim3").style.pointerEvents = "none";
+document.getElementById("mark1").checked = true; 
+document.getElementById("anim4").setAttribute("src", "/assets/images/yay_bunny2.gif");
+var myAudio = new Audio('/assets/audio/yay.mp3');
+document.getElementById("rew1").style.filter = "grayscale(0%)"
+myAudio.play();
+enableSubmit_env3()
+
+}
+
+function animal2()
+{
+  document.getElementById("anim1").style.border = "4px solid red"; 
+  document.getElementById("anim1").style.pointerEvents = "none";
+  document.getElementById("anim3").style.pointerEvents = "none";
+  document.getElementById("unmark1").checked = true; 
+  document.getElementById("anim4").setAttribute("src","/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_env3()
+}
+function animal3()
+{
+  document.getElementById("anim1").style.border = "4px solid red"; 
+  document.getElementById("anim2").style.pointerEvents = "none";
+  document.getElementById("anim1").style.pointerEvents = "none";
+  document.getElementById("unmark1").checked = true; 
+ document.getElementById("anim4").setAttribute("src","/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_env3()
+}
+
+//--------
+function animal4()
+{
+  document.getElementById("anim8").style.border = "4px solid red"; 
+  document.getElementById("anim9").style.pointerEvents = "none";
+  document.getElementById("anim8").style.pointerEvents = "none";
+  document.getElementById("unmark2").checked = true; 
+  document.getElementById("anim5").setAttribute("src", "/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_env3()
+}
+function animal5()
+{
+document.getElementById("anim8").style.border = "4px solid blue"; 
+document.getElementById("anim7").style.pointerEvents = "none";
+document.getElementById("anim9").style.pointerEvents = "none";
+document.getElementById("mark2").checked = true; 
+document.getElementById("anim5").setAttribute("src", "/assets/images/yay_bunny2.gif");
+var myAudio = new Audio('/assets/audio/yay.mp3');
+document.getElementById("rew2").style.filter = "grayscale(0%)"
+myAudio.play();
+enableSubmit_env3()
+
+}
+
+function animal6()
+{
+  document.getElementById("anim8").style.border = "4px solid red"; 
+  document.getElementById("anim7").style.pointerEvents = "none";
+  document.getElementById("anim9").style.pointerEvents = "none";
+  document.getElementById("unmark2").checked = true; 
+  document.getElementById("anim5").setAttribute("src", "/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_env3()
+}
+
+//-----
+
+function animal7()
+{
+  document.getElementById("anim15").style.border = "4px solid red"; 
+  document.getElementById("anim14").style.pointerEvents = "none";
+  document.getElementById("anim15").style.pointerEvents = "none";
+  document.getElementById("unmark3").checked = true; 
+  document.getElementById("anim6").setAttribute("src", "/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_env3()
+}
+
+function animal8()
+{
+  document.getElementById("anim15").style.border = "4px solid red"; 
+  document.getElementById("anim14").style.pointerEvents = "none";
+  document.getElementById("anim13").style.pointerEvents = "none";
+  document.getElementById("unmark3").checked = true; 
+  document.getElementById("anim6").setAttribute("src", "/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_env3()
+}
+
+function animal9()
+{
+document.getElementById("anim15").style.border = "4px solid blue"; 
+document.getElementById("anim14").style.pointerEvents = "none";
+document.getElementById("anim13").style.pointerEvents = "none";
+document.getElementById("mark3").checked = true; 
+document.getElementById("anim6").setAttribute("src", "/assets/images/yay_bunny2.gif");
+var myAudio = new Audio('/assets/audio/yay.mp3');
+document.getElementById("rew3").style.filter = "grayscale(0%)"
+myAudio.play();
+enableSubmit_env3()
+
+}
+
+//----
+
+function animal10()
+{
+document.getElementById("anim19").style.border = "4px solid blue"; 
+document.getElementById("anim20").style.pointerEvents = "none";
+document.getElementById("anim21").style.pointerEvents = "none";
+document.getElementById("mark4").checked = true; 
+document.getElementById("anim10").setAttribute("src", "/assets/images/yay_bunny2.gif");
+var myAudio = new Audio('/assets/audio/yay.mp3');
+document.getElementById("rew4").style.filter = "grayscale(0%)"
+myAudio.play();
+enableSubmit_env3()
+
+}
+
+function animal11()
+{
+  document.getElementById("anim19").style.border = "4px solid red"; 
+  document.getElementById("anim19").style.pointerEvents = "none";
+  document.getElementById("anim21").style.pointerEvents = "none";
+  document.getElementById("unmark4").checked = true; 
+  document.getElementById("anim10").setAttribute("src", "/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_env3()
+}
+function animal12()
+{
+  document.getElementById("anim19").style.border = "4px solid red"; 
+  document.getElementById("anim20").style.pointerEvents = "none";
+  document.getElementById("anim19").style.pointerEvents = "none";
+  document.getElementById("unmark4").checked = true; 
+  document.getElementById("anim10").setAttribute("src", "/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_env3()
+}
+
+function enableSubmit_env3(){
+  var chk = document.querySelectorAll('input[type="checkbox"]:checked').length;
+  if (chk == 4){
+       document.getElementById("submit").style.display="block";
+       document.getElementById("arrow1").style.display="block";
+        }
+  else{
+      document.getElementById("submit").style.display="none";
+        }
+        }
