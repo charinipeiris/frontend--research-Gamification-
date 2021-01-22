2fa471bca6eb1d10131b266f2e97f386916b4dec
Hirasha Pooliyadda <37568672+Hirasha@users.noreply.github.com>
2021-01-22 11:44:57 +0530

    completed G1 L1 aurudu game

--- src/app/grade1/environment/env-level1/env-grade1-level1-game4/env-grade1-level1-game4.component.ts
+++ src/app/grade1/environment/env-level1/env-grade1-level1-game4/env-grade1-level1-game4.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GamesService } from "../../../../services/games.service";
+import { EmotiondetectorComponent } from '../../../../emotiondetector/emotiondetector.component';
 
 @Component({
   selector: 'app-env-grade1-level1-game4',
@@ -6,10 +8,147 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./env-grade1-level1-game4.component.css']
 })
 export class EnvGrade1Level1Game4Component implements OnInit {
-
-  constructor() { }
+  totalmarks: any;
+  @ViewChild(EmotiondetectorComponent) child: EmotiondetectorComponent;
+  constructor(private gamesService: GamesService){}
 
   ngOnInit(): void {
   }
+  markstot() {
+    // document.getElementById("reset").click;
+    var time = document.getElementById("display").innerHTML;
+    // console.log(time);
+    document.getElementById("time-back").style.visibility = "hidden";
+    var clicksarray = document.getElementById("clickarray").innerHTML ;
+    // console.log(clicksarray);
+
+
+    var total = 0;
+
+    let element1 = <HTMLInputElement>document.getElementById("mark1");
+    let element2 = <HTMLInputElement>document.getElementById("mark2");
+    let element3 = <HTMLInputElement>document.getElementById("mark3");
+
+    if (element1.checked) {
+      total = total + 1;
+    }
+
+    if (element2.checked)
+      total = total + 1;
+
+    if (element3.checked)
+      total = total + 1;
+
+
+    // console.log(total);
+
+    // let username = localStorage.getItem('uname');
+    // this.gamesService.submitGame({    
+    //   completed_games : [
+    //   {
+    //   game_id : "E13",
+    //   marks : total,
+    //   time_spent : time,
+    //   emotions: this.child.emotions,
+    //   max_idle_time :  clicksarray
+    //   }
+    // ]},username).subscribe(res=>{console.log("success")}, err=>{console.log("error")});
+
+
+
+
+    var chk = document.querySelectorAll('input[type="checkbox"]:checked').length;
+    var modal = document.getElementById("myModal");
+    var subbtn = document.getElementById("submit");
+    if (chk == 3) {
+    
+
+  if (total == 3){
+modal.style.display = "block";
+subbtn.style.display = "none";
+setTimeout(function(){
+  document.getElementById('coin1').style.visibility = "visible";
+  },1000);
+
+setTimeout(function(){
+  document.getElementById('coin2').style.visibility = "visible";
+  },1500);
+
+setTimeout(function(){
+    document.getElementById('coin3').style.visibility = "visible";
+    },2000);
+    document.getElementById("perf-time").innerHTML = time;
+
+
+}
+else if (total == 2){
+modal.style.display = "block";
+setTimeout(function(){
+  document.getElementById('coin1').style.visibility = "visible";
+  },1000);
+
+setTimeout(function(){
+  document.getElementById('coin2').style.visibility = "visible";
+  },1500);
+
+  setTimeout(function(){
+    document.getElementById('coin3').style.visibility = "visible";
+    },1900);
+  document.getElementById('coin3').style.opacity ="0.3";
+
+    document.getElementById("perf-time").innerHTML = time;
+
+}
+
+else if(total == 1){
+modal.style.display = "block";
+  setTimeout(function(){
+    document.getElementById('coin1').style.visibility = "visible";
+    },1000);
+  
+    setTimeout(function(){
+    document.getElementById('coin2').style.visibility = "visible";
+      },1500);
+      document.getElementById('coin2').style.opacity ="0.3";
+
+    setTimeout(function(){
+      document.getElementById('coin3').style.visibility = "visible";
+        },1900);
+      document.getElementById('coin3').style.opacity ="0.3";
+
+        document.getElementById("perf-time").innerHTML = time;
+  
+    
+  
+}
+else if(total == 0){
+  modal.style.display = "block";
+  setTimeout(function(){
+    document.getElementById('coin1').style.visibility = "visible";
+    },1000);
+    document.getElementById('coin1').style.opacity ="0.3";
+  
+    setTimeout(function(){
+    document.getElementById('coin2').style.visibility = "visible";
+      },1500);
+      document.getElementById('coin2').style.opacity ="0.3";
+
+    setTimeout(function(){
+      document.getElementById('coin3').style.visibility = "visible";
+        },1900);
+      document.getElementById('coin3').style.opacity ="0.3";
+
+        document.getElementById("perf-time").innerHTML = time;
+
+  }
+
+}
+else {
+
+}
+
+
+this.child.endgame(); 
+}
 
 }
