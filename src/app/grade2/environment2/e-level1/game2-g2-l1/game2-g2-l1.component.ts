d1677bc6f0b8787aa222b5b953e5738e2397ab4a
charinipeiris <30485000+charinipeiris@users.noreply.github.com>
2021-01-28 02:17:34 +0530

    completed grade 2 level 1 food game partially

--- src/app/grade2/environment2/e-level1/game2-g2-l1/game2-g2-l1.component.ts
+++ src/app/grade2/environment2/e-level1/game2-g2-l1/game2-g2-l1.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GamesService } from "../../../../services/games.service";
+import { EmotiondetectorComponent } from '../../../../emotiondetector/emotiondetector.component';
+
 
 @Component({
   selector: 'app-game2-g2-l1',
@@ -7,9 +10,238 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Game2G2L1Component implements OnInit {
 
-  constructor() { }
+
+  totalmarks: any;
+  @ViewChild(EmotiondetectorComponent) child: EmotiondetectorComponent;
+  constructor(private gamesService: GamesService){}
 
   ngOnInit(): void {
   }
+  markstot_env1_g2l2_food() {
+    // document.getElementById("reset").click;
+    var time = document.getElementById("display").innerHTML;
+    // console.log(time);
+    document.getElementById("time-back").style.visibility = "hidden";
+    var clicksarray = document.getElementById("clickarray").innerHTML ;
+    // console.log(clicksarray);
+
+
+    var total = 0;
+    var final_total=0;
+    let element1 = <HTMLInputElement>document.getElementById("mark1");
+    let element2 = <HTMLInputElement>document.getElementById("mark2");
+    let element3 = <HTMLInputElement>document.getElementById("mark3");
+    let element4 = <HTMLInputElement>document.getElementById("mark4");
+    let element5 = <HTMLInputElement>document.getElementById("mark5");
+    let element6 = <HTMLInputElement>document.getElementById("mark6");
+   
+    if (element1.checked) {
+      total = total + 1;
+    }
+
+    if (element2.checked)
+      total = total + 1;
+
+    if (element3.checked)
+      total = total + 1;
+
+    if (element4.checked)
+      total = total + 1;
+
+    if (element5.checked)
+      total = total + 1;
+
+    if (element6.checked)
+      total = total + 1;
+
+   
+    // console.log(total);
+
+    if ( total==6 || total == 5){
+      final_total=3;
+    }
+    if ( total==4 || total == 3){
+      final_total=2;
+    }
+    if ( total==2 || total == 1){
+      final_total=1;
+    }
+    if (total==0){
+      final_total=0;
+    }
+    // let username = localStorage.getItem('uname');
+    // this.gamesService.submitGame({    
+    //   completed_games : [
+    //   {
+    //   game_id : "s12",
+    //   marks : final_total,
+    //   time_spent : time,
+    //   emotions: this.child.emotions,
+    //   max_idle_time :  clicksarray
+    //   }
+    // ]},username).subscribe(res=>{console.log("success")}, err=>{console.log("error")});
+
+
+
+
+    var chk = document.querySelectorAll('input[type="checkbox"]:checked').length;
+    var modal = document.getElementById("myModal");
+    var subbtn = document.getElementById("submit");
+    if (chk == 6) {
+
+
+      if (total == 6){
+        modal.style.display = "block";
+        subbtn.style.display = "none";
+        setTimeout(function(){
+          document.getElementById('coin1').style.visibility = "visible";
+          },1000);
+        
+        setTimeout(function(){
+          document.getElementById('coin2').style.visibility = "visible";
+          },1500);
+  
+        setTimeout(function(){
+                document.getElementById('coin3').style.visibility = "visible";
+                },1800);
+          
+                    document.getElementById("perf-time").innerHTML = time;
+        } 
+
+      if (total == 5){
+        modal.style.display = "block";
+        subbtn.style.display = "none";
+        setTimeout(function(){
+          document.getElementById('coin1').style.visibility = "visible";
+          },1000);
+        
+        setTimeout(function(){
+          document.getElementById('coin2').style.visibility = "visible";
+          },1500);
+  
+        setTimeout(function(){
+                document.getElementById('coin3').style.visibility = "visible";
+                },1800);
+          
+                    document.getElementById("perf-time").innerHTML = time;
+        } 
+
+        if (total == 4){
+          modal.style.display = "block";
+          subbtn.style.display = "none";
+          setTimeout(function(){
+            document.getElementById('coin1').style.visibility = "visible";
+            },1000);
+          
+          setTimeout(function(){
+            document.getElementById('coin2').style.visibility = "visible";
+            },1500);
+    
+          setTimeout(function(){
+                  document.getElementById('coin3').style.visibility = "visible";
+                    },1800);
+                  document.getElementById('coin3').style.opacity ="0.3";
+                   document.getElementById("perf-time").innerHTML = time;
+          }  
+          
+          if (total == 3){
+            modal.style.display = "block";
+            subbtn.style.display = "none";
+            setTimeout(function(){
+              document.getElementById('coin1').style.visibility = "visible";
+              },1000);
+            
+            setTimeout(function(){
+              document.getElementById('coin2').style.visibility = "visible";
+              },1500);
+      
+            setTimeout(function(){
+                    document.getElementById('coin3').style.visibility = "visible";
+                      },1800);
+                    document.getElementById('coin3').style.opacity ="0.3";
+                     document.getElementById("perf-time").innerHTML = time;
+            } 
+ 
+  
+    
+    if (total == 2){
+      modal.style.display = "block";
+      subbtn.style.display = "none";
+      setTimeout(function(){
+        document.getElementById('coin1').style.visibility = "visible";
+        },1000);
+      
+      setTimeout(function(){
+        document.getElementById('coin2').style.visibility = "visible";
+        },1500);
+        document.getElementById('coin2').style.opacity ="0.3";
+      
+      setTimeout(function(){
+            document.getElementById('coin3').style.visibility = "visible";
+              },1800);
+            document.getElementById('coin3').style.opacity ="0.3";
+      
+        document.getElementById('coin6').style.opacity ="0.3";
+        document.getElementById("perf-time").innerHTML = time;
+      
+      }
+  if (total == 1){
+modal.style.display = "block";
+subbtn.style.display = "none";
+setTimeout(function(){
+  document.getElementById('coin1').style.visibility = "visible";
+  },1000);
+
+setTimeout(function(){
+  document.getElementById('coin2').style.visibility = "visible";
+  },1500);
+  document.getElementById('coin2').style.opacity ="0.3";
+
+    document.getElementById("perf-time").innerHTML = time;
+setTimeout(function(){
+      document.getElementById('coin3').style.visibility = "visible";
+        },1800);
+      document.getElementById('coin3').style.opacity ="0.3";
+
+  document.getElementById('coin6').style.opacity ="0.3";
+  document.getElementById("perf-time").innerHTML = time;
 
 }
+
+
+else if(total == 0){
+  modal.style.display = "block";
+  setTimeout(function(){
+    document.getElementById('coin1').style.visibility = "visible";
+    },1000);
+    document.getElementById('coin1').style.opacity ="0.3";
+  
+  setTimeout(function(){
+    document.getElementById('coin2').style.visibility = "visible";
+      },1500);
+      document.getElementById('coin2').style.opacity ="0.3";
+        document.getElementById("perf-time").innerHTML = time;
+
+  setTimeout(function(){
+          document.getElementById('coin3').style.visibility = "visible";
+            },1800);
+          document.getElementById('coin3').style.opacity ="0.3";
+  
+           document.getElementById("perf-time").innerHTML = time;
+          
+
+  }
+
+}
+else {
+
+}
+
+
+this.child.endgame(); 
+}
+
+
+}
+
+
--- src/assets/js/envL1.js
+++ src/assets/js/envL1.js
@@ -129,7 +129,7 @@ function fin11()
   myAudio.play();
   enableSubmit_env2()
 }
-function meme12()
+function fin12()
 {
   document.getElementById("mem11").style.border = "4px solid red"; 
   document.getElementById("mem11").style.pointerEvents = "none";
@@ -817,4 +817,252 @@ function enableSubmit_g1env5(){
         }
 
 
-        
+   //==========================grade 2 level 1========================================
+   
+   
+   //===============game 2 food grade 2 level 1 ================================
+
+   
+function foodie1()
+{
+document.getElementById("mem1").style.border = "4px solid blue"; 
+document.getElementById("mem2").style.pointerEvents = "none";
+document.getElementById("mem3").style.pointerEvents = "none";
+document.getElementById("mark1").checked = true; 
+document.getElementById("mem13").setAttribute("src", "/assets/images/yay_bunny2.gif");
+var myAudio = new Audio('/assets/audio/yay.mp3');
+document.getElementById("rew1").style.filter = "grayscale(0%)"
+myAudio.play();
+enableSubmit_food2env2();
+}
+
+function foodie2()
+{
+  document.getElementById("mem1").style.border = "4px solid red"; 
+  document.getElementById("mem1").style.pointerEvents = "none";
+  document.getElementById("mem3").style.pointerEvents = "none";
+  document.getElementById("unmark1").checked = true; 
+  document.getElementById("mem13").setAttribute("src","/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_food2env2();
+}
+function foodie3()
+{
+  document.getElementById("mem1").style.border = "4px solid red"; 
+  document.getElementById("mem2").style.pointerEvents = "none";
+  document.getElementById("mem1").style.pointerEvents = "none";
+  document.getElementById("unmark1").checked = true; 
+ document.getElementById("mem13").setAttribute("src","/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_food2env2();
+}
+
+//--------2
+
+function foodie4()
+{
+  document.getElementById("mem6").style.border = "4px solid red"; 
+  document.getElementById("mem5").style.pointerEvents = "none";
+  document.getElementById("mem6").style.pointerEvents = "none";
+  document.getElementById("unmark2").checked = true; 
+  document.getElementById("amem14").setAttribute("src", "/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_food2env2();
+}
+function foodie6()
+{
+document.getElementById("mem6").style.border = "4px solid blue"; 
+document.getElementById("mem4").style.pointerEvents = "none";
+document.getElementById("mem5").style.pointerEvents = "none";
+document.getElementById("mark2").checked = true; 
+document.getElementById("mem14").setAttribute("src", "/assets/images/yay_bunny2.gif");
+var myAudio = new Audio('/assets/audio/yay.mp3');
+document.getElementById("rew2").style.filter = "grayscale(0%)"
+myAudio.play();
+enableSubmit_food2env2();
+
+}
+
+function foodie5()
+{
+  document.getElementById("mem6").style.border = "4px solid red"; 
+  document.getElementById("mem4").style.pointerEvents = "none";
+  document.getElementById("mem6").style.pointerEvents = "none";
+  document.getElementById("unmark2").checked = true; 
+  document.getElementById("mem14").setAttribute("src", "/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_food2env2();
+}
+
+//-----3
+
+function foodie7()
+{
+  document.getElementById("mem9").style.border = "4px solid red"; 
+  document.getElementById("mem8").style.pointerEvents = "none";
+  document.getElementById("mem9").style.pointerEvents = "none";
+  document.getElementById("unmark3").checked = true; 
+  document.getElementById("mem15").setAttribute("src", "/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_food2env2();
+}
+
+function foodie8()
+{
+  document.getElementById("mem9").style.border = "4px solid red"; 
+  document.getElementById("mem7").style.pointerEvents = "none";
+  document.getElementById("mem9").style.pointerEvents = "none";
+  document.getElementById("unmark3").checked = true; 
+  document.getElementById("mem15").setAttribute("src", "/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_food2env2();
+}
+
+function foodie9()
+{
+document.getElementById("mem9").style.border = "4px solid blue"; 
+document.getElementById("mem8").style.pointerEvents = "none";
+document.getElementById("mem7").style.pointerEvents = "none";
+document.getElementById("mark3").checked = true; 
+document.getElementById("mem15").setAttribute("src", "/assets/images/yay_bunny2.gif");
+var myAudio = new Audio('/assets/audio/yay.mp3');
+document.getElementById("rew3").style.filter = "grayscale(0%)"
+myAudio.play();
+enableSubmit_food2env2();
+
+}
+
+//----4
+
+function foodie10()
+{
+document.getElementById("mem11").style.border = "4px solid red"; 
+document.getElementById("mem11").style.pointerEvents = "none";
+document.getElementById("mem12").style.pointerEvents = "none";
+document.getElementById("unmark4").checked = true; 
+document.getElementById("mem16").setAttribute("src", "/assets/images/w.gif");
+var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+
+myAudio.play();
+enableSubmit_food2env2();
+
+}
+
+function foodie11()
+{
+  document.getElementById("mem11").style.border = "4px solid blue"; 
+  document.getElementById("mem10").style.pointerEvents = "none";
+  document.getElementById("mem12").style.pointerEvents = "none";
+  document.getElementById("mark4").checked = true; 
+  document.getElementById("mem16").setAttribute("src", "/assets/images/yay_bunny2.gif");
+  var myAudio = new Audio('/assets/audio/yay.mp3');
+  document.getElementById("rew4").style.filter = "grayscale(0%)"
+  myAudio.play();
+  enableSubmit_food2env2();
+}
+function foodie12()
+{
+  document.getElementById("mem11").style.border = "4px solid red"; 
+  document.getElementById("mem11").style.pointerEvents = "none";
+  document.getElementById("mem10").style.pointerEvents = "none";
+  document.getElementById("unmark4").checked = true; 
+  document.getElementById("mem16").setAttribute("src", "/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_food2env2();
+}
+//----5
+
+function foodie13()
+{
+document.getElementById("mem17").style.border = "4px solid blue"; 
+document.getElementById("mem18").style.pointerEvents = "none";
+document.getElementById("mem19").style.pointerEvents = "none";
+document.getElementById("mark5").checked = true; 
+document.getElementById("mem20").setAttribute("src", "/assets/images/yay_bunny2.gif");
+var myAudio = new Audio('/assets/audio/yay.mp3');
+document.getElementById("rew1").style.filter = "grayscale(0%)"
+myAudio.play();
+enableSubmit_food2env2();
+
+}
+
+function foodie14()
+{
+  document.getElementById("mem17").style.border = "4px solid red"; 
+  document.getElementById("mem17").style.pointerEvents = "none";
+  document.getElementById("mem19").style.pointerEvents = "none";
+  document.getElementById("unmark5").checked = true; 
+  document.getElementById("mem20").setAttribute("src","/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_food2env2();
+}
+function foodie15()
+{
+  document.getElementById("mem17").style.border = "4px solid red"; 
+  document.getElementById("mem17").style.pointerEvents = "none";
+  document.getElementById("mem18").style.pointerEvents = "none";
+  document.getElementById("unmark1").checked = true; 
+ document.getElementById("mem20").setAttribute("src","/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_food2env2();
+}
+
+
+//----6
+
+function foodie16()
+{
+  document.getElementById("mem22").style.border = "4px solid red"; 
+  document.getElementById("mem22").style.pointerEvents = "none";
+  document.getElementById("mem23").style.pointerEvents = "none";
+  document.getElementById("unmark6").checked = true; 
+  document.getElementById("mem24").setAttribute("src", "/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_food2env2();
+}
+function foodie17()
+{
+document.getElementById("mem22").style.border = "4px solid blue"; 
+document.getElementById("mem21").style.pointerEvents = "none";
+document.getElementById("mem23").style.pointerEvents = "none";
+document.getElementById("mark6").checked = true; 
+document.getElementById("mem24").setAttribute("src", "/assets/images/yay_bunny2.gif");
+var myAudio = new Audio('/assets/audio/yay.mp3');
+document.getElementById("rew2").style.filter = "grayscale(0%)"
+myAudio.play();
+enableSubmit_food2env2();
+
+}
+
+function foodie18()
+{
+  document.getElementById("mem22").style.border = "4px solid red"; 
+  document.getElementById("mem21").style.pointerEvents = "none";
+  document.getElementById("mem22").style.pointerEvents = "none";
+  document.getElementById("unmark6").checked = true; 
+  document.getElementById("mem24").setAttribute("src", "/assets/images/w.gif");
+  var myAudio = new Audio('/assets/audio/wrong-answer.mp3');
+  myAudio.play();
+  enableSubmit_food2env2();
+}
+
+function enableSubmit_food2env2(){
+  var chk = document.querySelectorAll('input[type="checkbox"]:checked').length;
+  if (chk == 6){
+       document.getElementById("submit").style.display="block";
+       document.getElementById("arrow1").style.display="block";
+        }
+  else{
+      document.getElementById("submit").style.display="none";
+        }
+        }
